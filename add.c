dc1617936238c5e03f7f1c4b5b41f076220a56b2
ygggzzz <183240667@qq.com>
2024-02-22 21:52:17 +0800

    更改

--- add.c
+++ add.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int a,b;
-    a=10;
-    b=20;
-    int c=a+b;
-    printf("%d",c);
+    int a, b;
+    a = 10;
+    b = 20;
+    int c = a + b;
+    int g = 0;
+    printf("%d", g);
+    printf("%d", c);
     return 0;
 }
